5c58ae17b30cd54140935d4f5720bfb2a1228d4b
dmarti61 <dmarti61@gmail.com>
2025-08-09 03:16:23 -0500

    Update page.jsx

--- src/app/dashboard/collegeguide/page.jsx
+++ src/app/dashboard/collegeguide/page.jsx
@@ -1,6 +1,6 @@
 // src/components/CollegeGuide.js
 import React from 'react';
-import '../../styles/collegeguide.css'; // Assuming this path is correct for your styles
+import '../../../styles/collegeguide.css'; // Assuming this path is correct for your styles
 
 const CollegeGuide = () => (
   <div className="college-guide-container">
