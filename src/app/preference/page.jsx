60af8af05b609d5cf2f0fd055b1ae78f5d66e273
dmarti61 <dmarti61@gmail.com>
2025-08-09 20:37:18 -0500

    Create page.jsx

---
+++ src/app/preference/page.jsx
@@ -0,0 +1,13 @@
+'use client'; 
+import React from 'react';
+import Preference from '../../components/quiz/preference';
+
+const Preference = () => {
+  return (
+    <main>
+      <PreferenceContent />
+    </main>
+  );
+};
+
+export default Preference;
