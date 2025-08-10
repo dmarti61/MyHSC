85681fec35f103e4da45ff60f20ec4446a2aeac4
dmarti61 <dmarti61@gmail.com>
2025-08-09 20:31:41 -0500

    Create page.jsx

---
+++ src/app/quiz/page.jsx
@@ -0,0 +1,13 @@
+'use client'; 
+import React from 'react';
+import QuizContent from './quiz-content'; 
+
+const QuizPage = () => {
+  return (
+    <main>
+      <QuizContent />
+    </main>
+  );
+};
+
+export default QuizPage;
