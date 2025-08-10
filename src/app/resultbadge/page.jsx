4332545ab868e591c745c674de189a0c25a4d40d
dmarti61 <dmarti61@gmail.com>
2025-08-09 20:39:08 -0500

    Create page.jsx

---
+++ src/app/resultbadge/page.jsx
@@ -0,0 +1,13 @@
+'use client'; 
+import React from 'react';
+import Results from '../../components/quiz/resultbadge';
+
+const Results = () => {
+  return (
+    <main>
+      <ResultContent />
+    </main>
+  );
+};
+
+export default Results;
