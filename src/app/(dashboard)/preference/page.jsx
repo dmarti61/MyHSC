05a6f4039f6336c25735acc9499517f8472d6a5e
dmarti61 <dmarti61@gmail.com>
2025-08-10 07:29:25 -0500

    Update page.jsx

--- src/app/(dashboard)/preference/page.jsx
+++ src/app/(dashboard)/preference/page.jsx
@@ -1,6 +1,6 @@
 'use client'; 
 import React, { Suspense } from 'react';
-import PreferenceContent from '../../components/quiz/preference.jsx';
+import PreferenceContent from '../../../components/quiz/preference.jsx';
 const Preferences = () => {
   return (
     <main>
