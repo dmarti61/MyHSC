d6d80e30592cbc9aafafdb4a4507da4b989f89d4
dmarti61 <dmarti61@gmail.com>
2025-08-10 20:28:02 -0500

    Update quiz.jsx

--- src/components/quiz/quiz.jsx
+++ src/components/quiz/quiz.jsx
@@ -3,8 +3,8 @@
 import React, { useState, useRef } from 'react';
 import { useRouter } from 'next/navigation'; // Import the Next.js router
 import questions from '@/lib/questions'; // Updated import path
-import QuestionCard from '@/components/questioncard';
-import ProgressBar from '@/components/progressbar';
+import QuestionCard from '@/components/quiz/questioncard';
+import ProgressBar from '@/components/quiz/progressbar';
 
 const mbtiDimensions = ['EI', 'SN', 'TF', 'JP'];
 
