845c38ec5acc91934d489300ee2937b490b2c28b
dmarti61 <dmarti61@gmail.com>
2025-08-11 10:46:34 -0500

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,11 +11,7 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}, {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
