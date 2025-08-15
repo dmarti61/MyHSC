2c0e6500c24ba1ed0119f9dd920420e28f4f3d39
dmarti61 <dmarti61@gmail.com>
2025-08-14 22:45:26 -0500

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,13 @@
 // src/app/layout.jsx
-'use client'; // This is required for using useState and useEffect
+'use client';
 
 import React, { useState, useEffect } from 'react';
 import Script from 'next/script';
 import Cookies from 'js-cookie';
 import '@/styles/global.css';
 import Footer from '@/components/footer';
-import CookieConsentBanner from '@/components/cookieconsentbanner';
-import { metadata as seoMetadata } from './metadata'; // Import the metadata
+import CookieConsentBanner from '@/components/CookieConsentBanner';
+import { metadata as seoMetadata } from './metadata';
 
 export default function RootLayout({ children }) {
   const [isConsentAccepted, setIsConsentAccepted] = useState(false);
@@ -25,6 +25,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        {/*
+          This script is strictly necessary as it loads the consent banner.
+          It must be loaded unconditionally to collect user consent.
+        */}
+        <Script async src="https://fundingchoicesmessages.google.com/i/pub-3588367397920518?ers=1" />
+        <Script id="google-funding-choices" strategy="afterInteractive">
+          {`
+            (function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();
+          `}
+        </Script>
+
         <main className="main-content">
           {children}
         </main>
@@ -32,6 +43,9 @@ export default function RootLayout({ children }) {
         
         <CookieConsentBanner onAccept={handleAccept} />
 
+        {/*
+          THESE SCRIPTS ARE NON-ESSENTIAL AND ARE BLOCKED UNTIL CONSENT IS GIVEN.
+        */}
         {isConsentAccepted && (
           <>
             <Script id="google-analytics" strategy="afterInteractive">
@@ -48,13 +62,6 @@ export default function RootLayout({ children }) {
               src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3588367397920518"
               crossOrigin="anonymous"
             />
-    
-            <Script async src="https://fundingchoicesmessages.google.com/i/pub-3588367397920518?ers=1" />
-            <Script id="google-funding-choices" strategy="afterInteractive">
-              {`
-                (function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();
-              `}
-            </Script>
           </>
         )}
       </body>
